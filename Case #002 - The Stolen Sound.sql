c41ce5ac670928dffee6b895ba6edf304bbed9f6
Hosam <34396668+hosamn@users.noreply.github.com>
2025-04-16 21:23:16 +0200

    Update Case #002 - The Stolen Sound.sql

--- Case #002 - The Stolen Sound.sql
+++ Case #002 - The Stolen Sound.sql
@@ -1,4 +1,8 @@
+SELECT name from suspects where id is (SELECT suspect_id from interviews where suspect_id in (SELECT id from suspects where bandana_color is "red" OR accessory is "gold watch") and transcript LIKE "%took%")
+
+
 SELECT * from witnesses where crime_scene_id is (select id from crime_scene WHERE location is "West Hollywood Records")
+
 SELECT * from suspects where bandana_color is "red" OR accessory is "gold watch"
 
-July 15, 1983
+SELECT * from interviews where suspect_id in (SELECT id from suspects where bandana_color is "red" OR accessory is "gold watch") and transcript is not null
