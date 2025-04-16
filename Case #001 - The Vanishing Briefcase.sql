ab5df7c9ac169cc345c74bca60d47bc66268b85c
Hosam <34396668+hosamn@users.noreply.github.com>
2025-04-16 20:33:41 +0200

    Update Case #001 - The Vanishing Briefcase.sql

--- Case #001 - The Vanishing Briefcase.sql
+++ Case #001 - The Vanishing Briefcase.sql
@@ -1,1 +1,6 @@
+SELECT name from suspects where id is (SELECT suspect_id from interviews where transcript Like "%steal%" and suspect_id in (SELECT id from suspects where scar is "left cheek" and attire is "trench coat"))
+
+
 SELECT * from interviews where suspect_id in (SELECT id from suspects where scar is "left cheek" and attire is "trench coat") and transcript is not null
+
+SELECT * from interviews where transcript Like "%steal%" and suspect_id in (SELECT id from suspects where scar is "left cheek" and attire is "trench coat")
