1dc517238e32431c9be86655738006b2ef0bb749
Hosam <34396668+hosamn@users.noreply.github.com>
2025-04-16 21:18:00 +0200

    Update Case #002 - The Stolen Sound.sql

--- Case #002 - The Stolen Sound.sql
+++ Case #002 - The Stolen Sound.sql
@@ -1,6 +1,4 @@
 SELECT * from witnesses where crime_scene_id is (select id from crime_scene WHERE location is "West Hollywood Records")
+SELECT * from suspects where bandana_color is "red" OR accessory is "gold watch"
 
-
-
-West Hollywood Records
 July 15, 1983
