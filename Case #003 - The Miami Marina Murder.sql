fad74d9af42bf9af598e79c234a07ae348fb934d
Hosam <34396668+hosamn@users.noreply.github.com>
2025-04-17 18:32:29 +0200

    Update Case #003 - The Miami Marina Murder.sql

--- Case #003 - The Miami Marina Murder.sql
+++ Case #003 - The Miami Marina Murder.sql
@@ -1,5 +1,10 @@
-
-
+SELECT *
+  FROM person
+  WHERE address LIKE "3% Ocean Drive%"
+  OR 
+  
+lives on 300ish "Ocean Drive"
+whose first name ends with "ul" and his last name ends with "ez".
 
 
 
@@ -8,3 +13,6 @@ SELECT *
   FROM crime_scene
   WHERE location IS "Coral Bay Marina"
   AND date IS "19860814"
+
+# id	date	location	description
+# 43	19860814	Coral Bay Marina	The body of an unidentified man was found near the docks. Two people were seen nearby: one who lives on 300ish "Ocean Drive" and another whose first name ends with "ul" and his last name ends with "ez".
