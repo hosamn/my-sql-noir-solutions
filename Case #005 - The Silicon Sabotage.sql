1277b13ded80c4ff10b35d75f7ed12e469000333
Hosam <34396668+hosamn@users.noreply.github.com>
2025-04-24 15:55:20 +0200

    Update Case #005 - The Silicon Sabotage.sql

--- Case #005 - The Silicon Sabotage.sql
+++ Case #005 - The Silicon Sabotage.sql
@@ -9,4 +9,16 @@ WHERE location LIKE "%QuantumTech%"
 -- id	date	location	description
 -- 74	19890421	QuantumTech HQ	Prototype destroyed; data erased from servers.
 
+SELECT *
+FROM witness_statements
+WHERE incident_id is (
+  SELECT id
+  FROM incident_reports
+  WHERE location LIKE "%QuantumTech%"
+  )
+
+-- id	incident_id	employee_id	statement
+-- 40	74	145	I heard someone mention a server in Helsinki.
+-- 59	74	134	I saw someone holding a keycard marked QX- succeeded by a two-digit odd number.
+
 
